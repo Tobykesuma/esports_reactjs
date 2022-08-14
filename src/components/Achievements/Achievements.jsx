71f87d32ad44077c24a55b886e7c340901dae7c7
Tobykesuma <102707093+Tobykesuma@users.noreply.github.com>
2022-08-14 14:27:08 +0800

    acheivements fix

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -13,29 +13,62 @@ import image from '../../constants/image';
 
 const Achievements = () => {
   return (
-    <section className='app__achievements section__padding' id='achievements'>
+    <section className='app__achievements' id='achievements'>
         <div className='app__achievements__headingtext'>
           <span>ACHIEVEMENTS</span>
         </div>
         <div className='app__achievements__content__images'>
           <Swiper
-            spaceBetween={10}
-            slidesPerView={5}
-            grabCursor={true}
-            loop={true}
-            autoplay={{
-              delay: 3500,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[
-              Pagination, Autoplay
-            ]}
+
+          spaceBetween={10}
+          slidesPerView={5}
+          grabCursor={true}
+          loop={true}
+          autoplay={{
+            delay: 4500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[
+            Pagination, Autoplay
+          ]}
+            // Breakpoints //
+            // breakpoints={{
+            //   // when window width is >= 650px
+            // 2000: {
+            //   width: 2000,
+            //   slidesPerView: 5,
+            // },
+
+            // 990: {
+            //   width: 990,
+            //   slidesPerView: 5,
+            // },
+
+            // 650: {
+            //   width: 650,
+            //   slidesPerView: 1,
+            // },
+            // 390: {
+            //   width: 390,
+            //   slidesPerView: 1,
+            // },
+
+
+            // }}
+
+            
           >
             <SwiperSlide>
                 <img src={image.Tournament_Logo1} className='app__achievements__img' alt='' />
+                <div className='app__achievements__content'>
+                  <div className='app__achievements__subtext'>
+                    <h1>1st</h1>
+                    <p>Place</p>
+                  </div>
+                </div>
             </SwiperSlide>
 
             <SwiperSlide>
